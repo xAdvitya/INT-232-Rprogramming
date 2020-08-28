41db5e60f6bc255272cb947a78d468dad9c60a07
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-08-28 15:32:37 +0530

    calc

---
+++ list.R
@@ -0,0 +1,7 @@
+lis = list("a"=2.6,"b"=FALSE,"c"=1:3)
+str(lis)
+length(lis)
+lis[-2]
+lis$a
+lis["a"]
+lis[["a"]]
---
+++ questions/calc.R
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+input= "+"
+result = switch(   
+    input,   
+    "+"=print(a + b),   
+    "-"=print(a - b),   
+    "/"=print(a / b),   
+    "%%"=print(a %% b), 
+    "*"=print(a * b),
+)   
+    
