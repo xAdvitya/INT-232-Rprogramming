7fda5cbf91afcea34332241424b63cf159a321c8
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-08-28 15:53:34 +0530

    order sort

---
+++ rep.R
@@ -0,0 +1,7 @@
+    x = 1:3
+    rep(x,times=3,each=2)
+
+    rep(x,1:3)
+
+mx = matrix(1:16,nrow=4,ncol=4)
+rep(mx,times=3)
---
+++ sort.r
@@ -0,0 +1,3 @@
+x = c(1,4,3,2,4,5,6,7,4,3,2)
+sort(x,decreasing=TRUE)
+order(x)
